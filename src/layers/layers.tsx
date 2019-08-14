7bda77ee78f767312cc76e5865f6f4aa17a6902e
awexfwex <6582665+awexfwex@users.noreply.github.com>
2019-08-14 08:36:13 -0400

    add linting to examples

--- src/layers/layers.tsx
+++ src/layers/layers.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-// I wish I can name it as 'layers', not 'Layers'
 export class Layers extends React.Component {
   public render() {
     return (<div>{this.props.children}</div>);
